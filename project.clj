80e1c44d6c29652c73ea343e58a85cee7e99b358
marco ponzi <marco.ponzi@gmail.com>
2016-02-16 18:37:08 +0100

    log4j added

--- project.clj
+++ project.clj
@@ -3,6 +3,12 @@
                  [ring/ring-devel "1.4.0"]
                  [ring/ring-core "1.4.0"]
                  [org.clojure/tools.logging "0.3.1"]
+[org.slf4j/slf4j-log4j12 "1.7.1"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]
                  [http-kit "2.1.19"]]
+  :resource-paths ["src/main/resource"] 
   :main "multipartws.core")
 
