b70ed05ba8a13ac39958185fa2651b51ead76408
marco ponzi <marco.ponzi@gmail.com>
2016-02-17 17:45:30 +0100

    multipart

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-devel "1.4.0"]
                  [ring/ring-core "1.4.0"]
+[javax.servlet/servlet-api "2.5"]
                  [org.clojure/tools.logging "0.3.1"]
 [org.slf4j/slf4j-log4j12 "1.7.1"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
--- src/multipartws/core.clj
+++ src/multipartws/core.clj
@@ -1,20 +1,50 @@
 (ns multipartws.core
   (:use [org.httpkit.server :only [run-server]])
   (:require [clojure.tools.logging :as log]
-            [ring.middleware.reload :as reload]))
+            [clojure.java.io :as io]
+            [ring.middleware.reload :as reload]
+            [ring.middleware.params :as params]
+            [ring.middleware.multipart-params :as multipart-params]))
 
-(defn handler [request]
-  (println "bef")
-  (log/info "TESTLOG")
-  (println "aft")
+(use 'ring.middleware.params
+     'ring.middleware.multipart-params)
+
+(defn handler [{params :params}] 
+  
+  (log/info "handler start")
+  (log/info (str "para name:" (params "name")))
+  (log/info (str "para file:" (params "file")))
+
+(let [{:keys [tempfile filename]} (get params "file")]
+  (io/copy tempfile (java.io.File. (str  "./" "RECEIVED" filename))) 
+  (log/info "copied:" filename))
+
+  (log/info "handler end")
+ 
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Hello Worldfdsafsafadsf"})
+   :body "Hello 1706"}
 
-
-(def app 
- handler
 )
+
+  (defn wrap-spy [handler]
+    (fn [request]
+      (println "-------------------------------")
+      (println "Incoming Request:")
+      (clojure.pprint/pprint request)
+      (let [response (handler request)]
+        (println "Outgoing Response Map:")
+        (clojure.pprint/pprint response)
+        (println "-------------------------------")
+        response)))
+
+
+(def app
+  (-> handler
+      params/wrap-params
+      multipart-params/wrap-multipart-params
+      (wrap-spy)))
+
 
 (defn -main [& args]
     (run-server (reload/wrap-reload #'app) {:port 8080}))
